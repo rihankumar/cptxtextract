500355ce4804a48591db2d121c97be18bc9b7c65
STuHacker <132763286+rihankumar@users.noreply.github.com>
2024-05-06 15:44:30 +0530

    Update details.py

--- details.py
+++ details.py
@@ -2,8 +2,8 @@ import os
 
 api_id = 22779671
 api_hash = "125d8d88b77309dc3b154cbbfc2dacb2"
-bot_token = "6847175705:AAHbkU8GFmzoxR9dCQTr6RuZ4NQsev5ufz0"
-auth_users = [1713924419]
-sudo_user = 1713924419
-log_channel = -1002143629860
-txt_channel = -1002049391187
+bot_token = "6499994285:AAEAxvQXn9isDP_myYf8dZQeb0wlJjoLRpU"
+auth_users = [1871785273]
+sudo_user = 1871785273
+log_channel = -1002132785565
+txt_channel = -1002064268488
