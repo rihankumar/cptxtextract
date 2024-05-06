09eef128232f94c61bad687ed37a52e0c331ecd9
STuHacker <132763286+rihankumar@users.noreply.github.com>
2024-05-06 15:57:36 +0530

    Update main.py

--- main.py
+++ main.py
@@ -325,7 +325,7 @@ try:
             
 
    
-    except Exception as error:
+    except Exception as error: -1002132785565
         LOGGER.error(f'Error: {error}')  # Log the error
         await m.reply(
             (
